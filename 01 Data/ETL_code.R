547b6e4df9bc92df5cce3a18b9b5dc707edd4912
Adam Barrett <ke5gpd@gmail.com>
2015-09-29 18:41:39 -0500

    no message

--- 01 Data/ETL_code.R
+++ 01 Data/ETL_code.R
@@ -2,18 +2,18 @@ require(tidyr)
 require(dplyr)
 require(ggplot2)
 
-setwd("~/Mine/UT/GitRepositories/DataVisualization/RWorkshop/04 Data ETL (Extract, Transform, and Load) in R/CSVs")
+setwd("~/DataVisualization/DV_RProject2/01 Data")
 
-file_path <- "April16AdvTblCalc.csv"
+file_path <- "hygdata_v3.csv"
 
 df <- read.csv(file_path, stringsAsFactors = FALSE)
 
 # Replace "." (i.e., period) with "_" in the column names.
 names(df) <- gsub("\\.+", "_", names(df))
 
-# str(df) # Uncomment this and  run just the lines to here to get column types to use for getting the list of measures.
+str(df) # Uncomment this and  run just the lines to here to get column types to use for getting the list of measures.
 
-measures <- c("Customer_ID", "Order_ID", "Postal_Code", "Row_ID", "Discount", "Gross_Profit_Ratio" , "Number_of_Records", "Order_Quantity", "Product_Base_Margin", "Profit", "Sales", "Shipping_Cost", "Unit_Price")
+measures <- c("id", "hip", "hd", "hr", "ra", "dec", "dist", "pmra", "pmdec", "rv", "mag", "absmag", "ci", "x", "y", "z", "vx", "vy", "vz", "rarad", "decrad", "pmrarad", "pmdecrad", "flam", "comp", "comp_primary", "lum", "var_min", "var_max")
 #measures <- NA # Do this if there are no measures.
 
 # Get rid of special characters in each column.
