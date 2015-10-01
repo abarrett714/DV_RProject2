b0fc87eb3ef56534dbd0ef0c6dfe66987091f6de
JDGrillo <jdgrillo@utexas.edu>
2015-09-30 22:10:15 -0500

    no message

--- 02 Data Wrangling/Workflow 2.R
+++ 02 Data Wrangling/Workflow 2.R
@@ -2,25 +2,25 @@ require("jsonlite")
 require("RCurl")
 require("ggplot2")
 
-df2 <- select(df, ID, DIST, CON, ABSMAG, MAG, LUM) %>% filter(CON == "Cet", LUM > 1000)
-df2
-summary(df2)
+dfJ <- select(df, ID, DIST, CON, ABSMAG, MAG, LUM) %>% mutate(lum_percent = cume_dist(LUM)) %>% filter(lum_percent >= .99, CON != "null")
+dfJ
+summary(dfJ)
 
 require(extrafont)
 ggplot() + 
   coord_cartesian() + 
-  scale_x_continuous() +
+  scale_x_discrete() +
   scale_y_continuous() +
   #facet_wrap(~SURVIVED) +
   #facet_grid(.~CON, labeller=label_both) + # Same as facet_wrap but with a label.
   #facet_grid(PCLASS~SURVIVED, labeller=label_both) +
   labs(title='Stars') +
-  labs(x="Star in Cet", y=paste("Luminosity")) +
-  layer(data=df2, 
-        mapping=aes(x=as.numeric(as.character(ID)), y=as.numeric(as.character(LUM))), 
+  labs(x="Distance from Earth", y=paste("lum_percent")) +
+  layer(data=dfJ, 
+        mapping=aes(as.character(DIST), y=as.numeric(as.character(lum_percent)), color = CON), 
         stat="identity", 
         stat_params=list(), 
-        geom="point",
+        geom="Point",
         geom_params=list(), 
         #position=position_identity()
         position=position_jitter(width=0.3, height=0)
