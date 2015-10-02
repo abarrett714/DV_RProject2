f903ad4b03bb3736f5990f73f760057bf43f1e64
Malachi Williams <malachiw@utexas.edu>
2015-10-01 22:14:29 -0500

    no message

--- 02 Data Wrangling/Workflow 4.R
+++ 02 Data Wrangling/Workflow 4.R
@@ -3,20 +3,20 @@ require("RCurl")
 require("ggplot2")
 require("dplyr")
 
-dfm <- select(df, ID, DIST, CON, ABSMAG, MAG, RV, LUM, VX) %>% filter(RV != 0, VX  != 0) %>% arrange(CON, DIST)
-dfm2 <- dfm %>% group_by(MAG) %>% summarise(n = n()) %>% arrange(n)
+dfm <- select(df, ID, DIST, CON, ABSMAG, MAG, RV, LUM, VX, VY, VZ) %>% filter(RV != 0, VX  != 0, RV >= -100, RV <= 100 ) %>% arrange(CON, DIST)
+dfm2 <- dfm %>% group_by(CON) %>% summarise(n = n()) %>% arrange(n)
 dfm2
   
   
 require(extrafont)
 ggplot() + 
   coord_cartesian() + 
-  scale_x_discrete() +
+  scale_x_continuous() +
   scale_y_continuous() +
   labs(title='Stars') +
   labs(x="Radial Velocity", y=paste("XVelocity"), color="Magnitude") +
   layer(data=dfm, 
-        mapping=aes(as.character(RV), y=as.numeric(as.character(VX)), color=MAG),
+        mapping=aes(as.numeric(as.character(RV)), y=as.numeric(as.character(VX)), color=MAG),
         stat="identity", 
         stat_params=list(), 
         geom="point",
